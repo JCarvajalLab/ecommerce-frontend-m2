e661d00329f8c808fa93b5c73a2e549000cb93f2
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-03-08 21:59:10 -0300

    se actualiza nuevamente y se crean 2 componentes navbar y footer, estos fueron agregados a las 5 paginas disponibles ya que causaba mucho problema cambiar una por una, ahora el codigo queda mas ordenado

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,18 +1,47 @@
-// Se agrega de forma adicional el metodo cambiar color de pantalla 
+// aplicar tema oscuro
 const temaOscuro = () => {
-    document.querySelector('body').setAttribute('data-bs-theme', 'dark'); // Cambiar a dark
-    document.querySelector('#dl-icon').setAttribute('class', 'bi bi-sun-fill'); // cambiar icono
+    document.querySelector('body').setAttribute('data-bs-theme', 'dark');
+    document.querySelector('#dl-icon').setAttribute('class', 'bi bi-sun-fill');
+    localStorage.setItem('tema', 'dark');
 }
 
+// aplicar tema claro
 const temaClaro = () => {
-    document.querySelector('body').setAttribute('data-bs-theme', 'light'); // Cambiar a claro
-    document.querySelector('#dl-icon').setAttribute('class', 'bi bi-moon-fill'); // cambiar icono
+    document.querySelector('body').setAttribute('data-bs-theme', 'light');
+    document.querySelector('#dl-icon').setAttribute('class', 'bi bi-moon-fill');
+    localStorage.setItem('tema', 'light');
 }
 
+// cambiar tema
 const cambiarTema = () => {
-    document.querySelector('body').getAttribute('data-bs-theme') === 'light' ?
-        temaOscuro() : temaClaro();
+    document.querySelector('body').getAttribute('data-bs-theme') === 'light'
+        ? temaOscuro()
+        : temaClaro();
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const temaGuardado = localStorage.getItem("tema");
+
+    if (temaGuardado === "dark") {
+        temaOscuro();
+    } else {
+        temaClaro();
+    }
+});
+
+
+// JS para cargar los componentes que se repiten como el navbar y el footer
+
+const cargarComponente = (ruta, elemento) => {
+    fetch(ruta)
+        .then(res => res.text())
+        .then(data => {
+            document.querySelector(elemento).innerHTML = data
+        })
+}
+
+cargarComponente("/assets/components/navbar.html", "#navbar")
+cargarComponente("/assets/components/footer.html", "#footer")
 
 // carrusel
 
