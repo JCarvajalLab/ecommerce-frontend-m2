1aa6d15cee74eada9cf5b20d15611ad25d59d538
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-03-08 21:06:54 -0300

    se actualiza carpeta componentesXpages, se agregan nuevas card a index.html y a productos, se agrega evento carrusel para cambiar productos desctacados

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,15 +1,42 @@
 // Se agrega de forma adicional el metodo cambiar color de pantalla 
-const temaOscuro = () =>{
-document.querySelector('body').setAttribute('data-bs-theme', 'dark'); // Cambiar a dark
-document.querySelector('#dl-icon').setAttribute('class', 'bi bi-sun-fill'); // cambiar icono
+const temaOscuro = () => {
+    document.querySelector('body').setAttribute('data-bs-theme', 'dark'); // Cambiar a dark
+    document.querySelector('#dl-icon').setAttribute('class', 'bi bi-sun-fill'); // cambiar icono
 }
 
-const temaClaro = () =>{
-document.querySelector('body').setAttribute('data-bs-theme', 'light'); // Cambiar a claro
-document.querySelector('#dl-icon').setAttribute('class', 'bi bi-moon-fill'); // cambiar icono
+const temaClaro = () => {
+    document.querySelector('body').setAttribute('data-bs-theme', 'light'); // Cambiar a claro
+    document.querySelector('#dl-icon').setAttribute('class', 'bi bi-moon-fill'); // cambiar icono
 }
 
 const cambiarTema = () => {
-    document.querySelector('body').getAttribute('data-bs-theme')=== 'light'?
-    temaOscuro() : temaClaro();
+    document.querySelector('body').getAttribute('data-bs-theme') === 'light' ?
+        temaOscuro() : temaClaro();
 }
+
+// carrusel
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const carousel = document.getElementById("productCarousel");
+    const btnRight = document.querySelector(".carousel-btn-right");
+    const btnLeft = document.querySelector(".carousel-btn-left");
+
+    btnRight.addEventListener("click", () => {
+
+        const firstCard = carousel.firstElementChild;
+
+        carousel.appendChild(firstCard);
+
+
+    });
+
+    btnLeft.addEventListener("click", () => {
+
+        const lastCard = carousel.lastElementChild;
+
+        carousel.prepend(lastCard);
+
+    });
+
+});
